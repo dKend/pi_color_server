bc4697c812ed19713b11794dbc4357c8992553cb
dKend <dhk@uwm.edu>
2017-09-01 11:10:49 -0500

    Add handleColor.php

---
+++ src/var/www/html/handleColor.php
@@ -0,0 +1,5 @@
+<?php 
+	$red = $_GET["red"];
+	$green = $_GET["green"];
+	$blue = $_GET["blue"];
+?>
--- src/var/www/html/index.php
+++ src/var/www/html/index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <?php echo "hello world"; ?>
-	<form id = "rgb_sliders" method = "GET">
+	<form id = "rgb_sliders" method = "GET" action="handleColor.php">
 		R: <input name = "red" type = "range" min = "0" max = "255"/><br>
 		G: <input name = "green" type = "range" min = "0" max = "255"/><br>
 		B: <input name = "blue" type = "range" min = "0" max = "255"/><br>

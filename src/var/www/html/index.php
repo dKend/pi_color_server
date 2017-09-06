63686a4147fff69866010cb77890eeb3e91007a3
dKend <dhk@uwm.edu>
2017-09-06 05:57:23 -0500

    2
    2

--- src/var/www/html/index.php
+++ src/var/www/html/index.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html>
 <body>
-<?php echo "hello world"; ?>
+<?php
+	//not the same for every system
+	// 4 is integer byte length (32-bit unsigned integer) on raspberry pi model b using raspbian OS
+	$INT_BYTES = 4;
+	
+	function getColor(resource $sock)
+	{
+		socket_write($sock,pack("I", $INT_BYTES));
+		$r = unpack(socket_read($sock, $INT_BYTES));
+		$g = unpack(socket_read($sock, $INT_BYTES));
+		$b = unpack(socket_read($sock, $INT_BYTES));
+		
+	}
+	
+	function connectToColorServer()
+	{
+		if(chdir("/"))
+		{
+			echo "Directory changed Successfully.";
+			$sock = socket_create(AF_UNIX, SOCK_STREAM);
+			if($sock == false)
+			{
+				echo "Socket creation failed.";
+			}else
+			{
+				socket_connect($sock, "colorserver", 0);
+				return($sock);
+			}
+		}else
+		{
+			echo "Directory Change Failed."
+		}
+		return(false);
+	}
+?>
 	<form id = "rgb_sliders" method = "GET" action="handleColor.php">
 		R: <input name = "red" type = "range" min = "0" max = "255"/><br>
 		G: <input name = "green" type = "range" min = "0" max = "255"/><br>
 		B: <input name = "blue" type = "range" min = "0" max = "255"/><br>
-		<input type = "submit"/>
+		<input name = "stclr" type = "submit"/>
 	</form>
 </body>
 </html>

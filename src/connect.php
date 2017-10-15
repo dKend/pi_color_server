fda08e396424c3175ae02b38254da05b4ff58af4
dKend <dhk@uwm.edu>
2017-10-15 18:35:13 -0500

    Created auth.php, connect.php, and Member.class.php
    Created auth.php
    Created connect.php
    Created Member.class.php

--- src/connect.php
+++ src/connect.php
@@ -1,11 +1,10 @@
 <?php
-	$username = "";
-	$password = "";
-	$servername = "localhost";
-	$dbname = "dhkDB";
+	function connectToDB()
+	{
+		$config = parse_ini_file("../conf.ini");
+		$conn = new PDO("mysql:host=".$config['servername'].";dbname=".$config['dbname'], $config['username'], $config['password']);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return($conn);
 	
-	try {
-		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	}//unfinished try statement
-	
+	}
 ?>
